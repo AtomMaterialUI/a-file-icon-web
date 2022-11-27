b97cb29d9b94b7bcf39070af7d69f8c76eb3f0c9
Elior <heliosaian@gmail.com>
2022-11-27 18:03:21 +0200

    fix azure fatal error

--- src/providers/azure.ts
+++ src/providers/azure.ts
@@ -1,4 +1,9 @@
 import { AbstractProvider } from '~providers/AbstractProvider';
+import { iconSize } from '~common/storage';
+import { getFolderAssociation, getFolderIconName, getFolderIcon } from '~associations/folders';
+import { wrapSvg, removeSize } from '~associations/utils';
+import { getAssociation, getFileIconName, getFileIcon } from '~associations/files';
+import select from 'select-dom';
 
 export class AzureProvider extends AbstractProvider {
   public get dirClass(): string {
@@ -28,5 +33,56 @@ export class AzureProvider extends AbstractProvider {
   public get styles(): string {
     return '';
   }
+
+  injectIcons = async () => {
+    const $items = select.all(this.itemsClass, this.target);
+    const isDark = select('html').dataset['colorMode'] === 'dark';
+    const size = Number(await iconSize() || 20);
+    // const color = await iconColor();
+
+    $items.forEach(async (item, index) => {
+      const isFile = select.exists(this.fileClass, item);
+      const isDir = select.exists(this.dirClass, item);
+      const isSvg = select.exists(this.svgClass, item);
+
+      const name = select(this.nameClass, item)?.textContent?.trim();
+      const $icon = select(this.iconClass, item);
+
+      if (isDir) {
+        let assoc = getFolderAssociation(name);
+        let className = getFolderIconName(assoc);
+
+        const svg = getFolderIcon(className);
+        const icon = await wrapSvg(svg, this.styles, `octicon ${this.dirClass}`);
+
+        const newSVG = document.createElement('span');
+        newSVG.innerHTML = removeSize(icon);
+
+        if ($icon.hasChildNodes()) {
+          $icon.replaceChild(newSVG, $icon.firstChild);
+        }
+        else {
+          $icon.appendChild(newSVG);
+        }
+      }
+      else if (isFile || isSvg) {
+        let assoc = getAssociation(name);
+        let className = getFileIconName(assoc);
+
+        const svg = getFileIcon(className, isDark);
+        const icon = await wrapSvg(svg, this.styles, `octicon ${this.fileClass}`);
+
+        const newSVG = document.createElement('span');
+        newSVG.innerHTML = removeSize(icon);
+
+        if ($icon.hasChildNodes()) {
+          $icon.replaceChild(newSVG, $icon.firstChild);
+        }
+        else {
+          $icon.appendChild(newSVG);
+        }
+      }
+    });
+  };
 
 }
